95a1418cb121b9008357330b799f4871961b240a
phgnm <phuongnam710648@gmail.com>
2024-11-02 12:14:17 +0700

    initialized a main menu

---
+++ src/src/menu/mainMenu/mainMenu.java
@@ -0,0 +1,37 @@
+package menu.mainMenu;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mainMenu extends JFrame implements ListSelectionListener, ActionListener {
+    private JPanel mainPanel;
+    private JButton historyButton;
+    private JButton addButton;
+    private JButton resetButton;
+    private JButton randomButton;
+    private JButton quizButton;
+    private JButton searchButton;
+    private JTextField searchField;
+    private JComboBox typeSearch;
+    private JTable tableSearch;
+    private JLabel executionTime;
+
+    public static void main(String[] args) {
+        ;
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        ;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ;
+
+    }
+}
