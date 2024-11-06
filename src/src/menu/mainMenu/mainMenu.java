122bc6780f1ab0574940f6563a7dbb67e029c754
phgnm <phuongnam710648@gmail.com>
2024-11-06 11:12:55 +0700

    runnable app

--- src/src/menu/mainMenu/mainMenu.java
+++ src/src/menu/mainMenu/mainMenu.java
@@ -3,6 +3,7 @@ package menu.mainMenu;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -27,9 +28,39 @@ public class mainMenu extends JFrame implements ListSelectionListener, ActionLis
     public mainMenu(String title) {
         super(title);
 
+        searchField = new JTextField();
+        searchButton = new JButton("Search");
+        typeSearch = new JComboBox();
+        tableSearch = new JTable();
+        mainPanel = new JPanel();
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
+        searchField.setMaximumSize(new Dimension(150, 30));
+        searchField.setPreferredSize(new Dimension(150, 30));
+        typeSearch.setMaximumSize(new Dimension(150, 30));
+        typeSearch.setPreferredSize(new Dimension(150, 30));
+        searchButton.setMaximumSize(new Dimension(150, 30));
+        searchButton.setPreferredSize(new Dimension(150, 30));
+        String column[] = {"ID", "Slang", "Definition"};
+        tableSearch.setModel(new DefaultTableModel(column, 0));
+        tableSearch.setRowHeight(30);
+        model = (DefaultTableModel) tableSearch.getModel();
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        tableSearch.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
+        tableSearch.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
+        tableSearch.getColumnModel().getColumn(2).setCellRenderer(centerRenderer);
+        ListSelectionModel selectionModel = tableSearch.getSelectionModel();
+        selectionModel.addListSelectionListener(this);
+        
+        mainPanel.add(searchField);
+        mainPanel.add(typeSearch);
+        mainPanel.add(searchButton);
+        mainPanel.add(new JScrollPane(tableSearch));
 
+        this.add(mainPanel);
+        this.pack();
     }
     public static void main(String[] args) {
         mainMenu app = new mainMenu("Dictionary");
@@ -52,7 +83,10 @@ public class mainMenu extends JFrame implements ListSelectionListener, ActionLis
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == searchButton) {
-            ;
+            String key = searchField.getText();
+            if (key.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Please enter a slang word!");
+            }
         }
     }
 }
